632516092c63247b820f2552339e31ccb7da21c3
pkelaita <pierce@kelaita.com>
2020-09-25 12:45:33 -0700

    code quality

--- tests/test_db.py
+++ tests/test_db.py
@@ -8,11 +8,7 @@ from unittest.mock import patch
 @patch('pymongo.MongoClient')
 def test_connection(mock_MongoClient):
     ret_val = mongomock.MongoClient()
-
-    def mocked_command(_cmd):
-        return None
-
-    ret_val[DB_NAME].command = mocked_command
+    ret_val[DB_NAME].command = lambda _cmd: None
     mock_MongoClient.return_value = ret_val
     with DBConnection() as conn:
         print(conn.client)
