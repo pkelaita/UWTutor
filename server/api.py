b06561695da446b968f68fb40384d9e4d95fc30f
pkelaita <pierce@kelaita.com>
2020-12-31 11:47:24 -0800

    added db connection in api

--- server/api.py
+++ server/api.py
@@ -2,24 +2,29 @@ from flask import Flask, make_response  # , request
 from flask_cors import CORS
 import json
 
+from db.connection import DBConnection
+import config
+
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 CORS(app)
 
 
 def tojson(res):
     r = make_response(json.dumps(res))
-    r.mimetype = "application/json"
+    r.mimetype = 'application/json'
     return r
 
 
-@app.route("/", methods=["GET"])
-def root():
-    obj = {
-        "foo": "bar",
-        "a": [1, 2, 3],
-    }
-    return tojson(obj)
+@app.route('/dbinfo', methods=['GET'])
+def info():
+    info = {}
+    with DBConnection() as conn:
+        db = conn.db
+        info['db_host'] = db.client.HOST
+        info['db_port'] = db.client.PORT
+        info['collections'] = db.list_collection_names()
+    return tojson(info)
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host=config.API_HOST, port=config.API_PORT)
